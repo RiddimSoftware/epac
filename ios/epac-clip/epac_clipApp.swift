2d937fda950cbb473a5838ac5f2b8fce4fba8c14
sunny <sunny@riddimsoftware.com>
2026-04-27 06:23:41 -0400

    review: fix epac_clipApp to call ContentView() with no args; flag unverified App Store ID

--- ios/epac-clip/epac_clipApp.swift
+++ ios/epac-clip/epac_clipApp.swift
@@ -6,25 +6,12 @@
 //
 
 import SwiftUI
-import SwiftData
 
 @main
 struct epac_clipApp: App {
-	var sharedModelContainer: ModelContainer = {
-		let schema = Schema(versionedSchema: SchemaV3.self)
-		let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: true)
-
-		do {
-			return try ModelContainer(for: schema, configurations: [modelConfiguration])
-		} catch {
-			fatalError("Could not create ModelContainer: \(error)")
-		}
-	}()
-
 	var body: some Scene {
 		WindowGroup {
-			ContentView(modelContainer: sharedModelContainer)
+			ContentView()
 		}
-		.modelContainer(sharedModelContainer)
 	}
 }
